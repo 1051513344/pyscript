eec72790150db5110612e05bb7f6cf525b982124
徐顺洁 <xushunjie@zhuojianchina.com>
2023-01-30 16:57:26 +0800

    javabean属性复制类型更正

---
+++ test/javabean属性复制类型更正/test.py
@@ -0,0 +1,27 @@
+
+
+
+with open("ResidentStudentVo.java", "r", encoding="utf-8") as f:
+    Vo = f.read()
+
+with open("TurnInterStdPlan.java", "r", encoding="utf-8") as f:
+    Do = f.read()
+
+VoDict = {}
+for row in Vo.split("\n"):
+    if row.startswith("    private"):
+        rl = row.replace("    private ", "").replace(";", "").split(" ")
+        VoDict[rl[1]] = rl[0]
+
+DoDict = {}
+for row in Do.split("\n"):
+    if row.startswith("    private"):
+        rl = row.replace("    private ", "").replace(";", "").split(" ")
+        DoDict[rl[1]] = rl[0]
+
+
+for k,v in DoDict.items():
+    if k in VoDict:
+        if DoDict[k] != VoDict[k]:
+            print(k, VoDict[k], DoDict[k])
+
