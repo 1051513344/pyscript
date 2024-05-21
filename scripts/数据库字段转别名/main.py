308ec2a5464b2bf915d28aa2a4e9770996fa23d1
徐顺洁 <xushunjie@ewell.cc>
2024-05-21 16:23:54 +0800

    相关脚本代码提交

--- scripts/数据库字段转别名/main.py
+++ scripts/数据库字段转别名/main.py
@@ -1,9 +1,15 @@
 
 
-columns = """    <sql id="Base_Column_List">
-        ID, BUSINESS_TYPE, ANNUAL, TIME_TYPE, WARD_ID, WARD_NAME, RESPONSIBLE_ID, RESPONSIBLE_NAME, STATUS,
-        CREATE_ID, CREATE_TIME, UPDATE_ID, UPDATE_TIME, VALID_FLAG, WRITE_BY
-    </sql>"""
+columns = """  <sql id="Base_Column_List">
+    ID, FORM_TYPE, CHECK_TYPE_ID, CHECK_TYPE_NAME, CHECK_STANDARD_ID, CHECK_STANDARD_NAME, 
+    FEEDBACK_NUM, CREATE_ID, CREATE_TIME, UPDATE_ID, UPDATE_TIME, VALID_FLAG,QC_DATA_DETAIL_DICT_ID,
+    SERIAL_NUMBER, VERSION, RELEASE_STATUS, RELEASE_TIME, EDITOR_ID, EDITOR_NAME, REVIEWER_ID, REVIEWER_NAME,
+    RANGE_TYPE,TOUCH_WARD_IDS,TOUCH_WARD_INFO,TOUCH_DEPT_IDS,UNIT_TYPE,UNIT_ID,QUALIFIED_SCORE,
+    STANDARD_TYPE, STANDARD_CATEGORY, SUPPORT_TEST_PAPER,SYSTEM_FLAG,STANDARD_STYLE,INDICATOR_CODE, COLLECT_WAY,
+    SPECIAL_TEAM,PATIENT_CHECKUP_FORM,MIN_CHECK_PEOPLE_NUM,
+    NURSE_CHECKUP_FORM,NURSE_MIN_CHECK_PEOPLE_NUM,
+    SCORING_TYPE,MIN_INS_FREQUENCY,ITEM_PROGRESSION,QC_INDEX_DICT_IDS,OPEN_QC_REQUIREMENTS,QC_REQUIREMENTS_CONTENT
+  </sql>"""
 
 
 
@@ -15,8 +21,8 @@ if __name__ == "__main__":
     for row in columns.split("\n")[1:-1]:
         line = row.replace("        ", "")
         for column in line.split(","):
-            if column != "":
-                alias_columns = alias_columns + "qt." + column.strip() + ", "
+            if column != "" and column != " ":
+                alias_columns = alias_columns + "SF." + column.strip() + ", "
             else:
                 alias_columns = alias_columns + "\n        "
     alias_columns = alias_columns[:-2] + "\n    </sql>"
