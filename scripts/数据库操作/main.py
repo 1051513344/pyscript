ee355bd83ab317411ab1da7984fe26e445635c7b
徐顺洁 <xushunjie@ewell.cc>
2024-06-05 10:42:54 +0800

    oracle连接预设

--- scripts/数据库操作/main.py
+++ scripts/数据库操作/main.py
@@ -4,6 +4,22 @@ from DBClient import *
 
 if __name__ == "__main__":
 
+    # username = ""
+    # password = ""
+    # host = ""
+    # port = 1521
+    # service = ""
+    # client = OracleClient(username, password, host, port, service)
+    # sql = """SELECT * FROM (
+    #       SELECT t.*
+    #       FROM HLZK.MENU t
+    #       WHERE NAME = '全院敏感指标追踪表'
+    #   ) WHERE ROWNUM <= 101"""
+    # result = client.selectList(sql)
+    # client.close()
+    # print(result)
+
+
     username = ""
     password = ""
     host = ""
