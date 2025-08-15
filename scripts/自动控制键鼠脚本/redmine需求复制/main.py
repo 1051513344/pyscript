52ea41d9abdee4bbee308d8f8958ae4d81c3346b
徐顺洁 <xushunjie@ewell.cc>
2025-08-15 17:23:50 +0800

    redmine需求复制

---
+++ scripts/自动控制键鼠脚本/redmine需求复制/main.py
@@ -0,0 +1,49 @@
+import pydirectinput
+import time
+
+def main():
+    openTask()
+    start = 0
+    for task in range(start, 20):
+        pydirectinput.mouseDown(x=650, y=365+task*21)
+        pydirectinput.mouseUp(x=650, y=365+task*21)
+        time.sleep(0.3)
+        pydirectinput.click(button='right')
+        if task < 16:
+            pydirectinput.mouseDown(x=700, y=365+task*21+130)
+            pydirectinput.mouseUp(x=700, y=365+task*21+130)
+        else:
+            pydirectinput.mouseDown(x=700, y=365+16*21+120)
+            pydirectinput.mouseUp(x=700, y=365+16*21+120)
+        if task == start:
+            tab(2)
+        else:
+            tab(1)
+        copyTaskContent()
+        tab(1)
+
+def openTask():
+    tab(1)
+
+def copyTaskContent():
+    pydirectinput.keyDown('ctrl')
+    time.sleep(0.2)
+    pydirectinput.keyDown('v')
+    time.sleep(0.2)
+    pydirectinput.keyUp('v')
+    time.sleep(0.2)
+    pydirectinput.keyUp('ctrl')
+    pydirectinput.keyDown('enter')
+    pydirectinput.keyUp('enter')
+
+def tab(num):
+    pydirectinput.keyDown('alt')
+    time.sleep(0.2)
+    for n in range(num):
+        pydirectinput.keyDown('tab')
+        time.sleep(0.2)
+        pydirectinput.keyUp('tab')
+    pydirectinput.keyUp('alt')
+
+if __name__ == "__main__":
+    main()
